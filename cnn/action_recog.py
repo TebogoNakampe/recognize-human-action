1ca46c86005d111f5094ccc8f5ab201dd382bc11
Byeongjo <bjbj8083@gmail.com>
2017-11-15 22:30:47 +0900

    for backup

--- cnn/action_recog.py
+++ cnn/action_recog.py
@@ -1,87 +1,83 @@
 import tensorflow as tf
 import numpy as np
 
-
-
 def run(data, batch_size, epoch):
-	train_x = data[0]
-	train_y = data[1]
-	test_x = data[2]
-	test_y = data[3]
-	print(train_x.shape)
-	print(train_y[0].shape)
+    train_x = data[0]
+    train_y = data[1]
+    test_x = data[2]
+    test_y = data[3]
+    print(train_x.shape)
+    print(train_y.shape)
+    print(test_x.shape)
+    print(test_y.shape)
 
-	train_len = len(train_x)
-	num_out = len(train_y[0])
+    train_len = len(train_x)
+    num_out = len(train_y[0])
 
-	train_y = train_y.reshape(-1, num_out)
-	test_y = test_y.reshape(-1, num_out)
-	#X -> fix
-	#Y -> can be changed
-	X = tf.placeholder(tf.float32, [None, 120, 160, 3])
-	Y = tf.placeholder(tf.float32, [None, num_out])
+    train_y = train_y.reshape(-1, num_out)
+    test_y = test_y.reshape(-1, num_out)
+#    X -> fix
+#    Y -> can be changed
+    X = tf.placeholder(tf.float32, [None, 120, 160, 3])
+    Y = tf.placeholder(tf.float32, [None, num_out])
 
-	W1 = tf.Variable(tf.random_normal([5, 5, 3, 6], stddev=0.01))
-	C1 = tf.nn.conv2d(X, W1, strides=[1, 1, 1, 1], padding='SAME') #no padding -> edge == trash data
-	L1 = tf.nn.relu(C1)
-	P1 = tf.nn.max_pool(L1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-	#P1 -> (?, 60, 80, 6)
+    W1 = tf.Variable(tf.random_normal([5, 5, 3, 6], stddev=0.01))
+    C1 = tf.nn.conv2d(X, W1, strides=[1, 1, 1, 1], padding='SAME')
+#    no padding -> edge == trash data
+    L1 = tf.nn.relu(C1)
+    P1 = tf.nn.max_pool(L1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+#    P1 -> (?, 60, 80, 6)
 
-	W2 = tf.Variable(tf.random_normal([5, 5, 6, 12], stddev=0.01))
-	C2 = tf.nn.conv2d(P1, W2, strides=[1, 1, 1, 1], padding='SAME')
-	L2 = tf.nn.relu(C2)
-	P2 = tf.nn.max_pool(L2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-	#P2 -> (?, 30, 40, 12)
+    W2 = tf.Variable(tf.random_normal([5, 5, 6, 12], stddev=0.01))
+    C2 = tf.nn.conv2d(P1, W2, strides=[1, 1, 1, 1], padding='SAME')
+    L2 = tf.nn.relu(C2)
+    P2 = tf.nn.max_pool(L2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+#    P2 -> (?, 30, 40, 12)
 
-	W3 = tf.Variable(tf.random_normal([30*40*12, 30*40*12], stddev=0.01))
-	L3 = tf.reshape(P2, [-1, 30*40*12])
-	L3 = tf.nn.relu(tf.matmul(L3, W3))
+    W3 = tf.Variable(tf.random_normal([30*40*12, 30*40*12], stddev=0.01))
+    L3 = tf.reshape(P2, [-1, 30*40*12])
+    L3 = tf.nn.relu(tf.matmul(L3, W3))
 
-	W4 = tf.Variable(tf.random_normal([30*40*12, num_out], stddev=0.01))
-	L4 = tf.matmul(L3, W4)
-	model = tf.nn.softmax(L4)
+    W4 = tf.Variable(tf.random_normal([30*40*12, num_out], stddev=0.01))
+    L4 = tf.matmul(L3, W4)
+    model = tf.nn.softmax(L4)
 
-	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=model, labels=Y))
-	optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=model, labels=Y))
+    optimizer = tf.train.AdamOptimizer(0.01).minimize(cost)
 
-	init = tf.global_variables_initializer()
-	sess = tf.Session()
-	sess.run(init)
+    init = tf.global_variables_initializer()
+    sess = tf.Session()
+    sess.run(init)
 
+    total_batch = int(train_len/batch_size)
+    if(total_batch == 0):
+        total_batch = 1
+    for e in range(epoch):
+        total_cost = 0
 
-	total_batch = int(train_len/batch_size)
-	if(total_batch == 0):
-		total_batch = 1
-	for e in range(epoch):
-		total_cost = 0
+        j = 0
+        for i in range(total_batch):
+            if(j+batch_size > len(train_x)):
+                batch_x = train_x[j:]
+                batch_y = train_y[j:]
+            else:
+                batch_x = train_x[j:j+batch_size]
+                batch_y = train_y[j:j+batch_size]
+                j = j+batch_size
 
-		j = 0
-		for i in range(total_batch):
-			if(j+batch_size > len(train_x)):
-				batch_x = train_x[j:]
-				batch_y = train_y[j:]
-			else:
-				batch_x = train_x[j:j+batch_size]
-				batch_y = train_y[j:j+batch_size]
-				j = j+batch_size
-				
-				
-			batch_y = batch_y.reshape(-1, num_out)
-				#print(type(batch_x))
-				#print(batch_x.shape)
-				#print(type(batch_y))
-				#print(batch_y.shape)
-			_, cost_val = sess.run([optimizer, cost], feed_dict={X:batch_x, Y:batch_y})
+            batch_y = batch_y.reshape(-1, num_out)
+#            print(type(batch_x))
+#            print(batch_x.shape)
+#            print(type(batch_y))
+#            print(batch_y.shape)
+            _, cost_val = sess.run([optimizer, cost], feed_dict={X:batch_x, Y:batch_y})
 
-			total_cost = total_cost + cost_val
+            total_cost = total_cost + cost_val
 
-		print('Epoch:', '%04d' % (epoch + 1), 'Avg. cost =', '{:.3f}'.format(total_cost / total_batch))
+        print('Epoch:', '%04d' % (e + 1), 'Average cost =', '{:.3f}'.format(total_cost / total_batch))
 
-	print("complete")
+    print("complete")
 
-
-	is_correct = tf.equal(tf.argmax(model, 1), tf.argmax(Y, 1))
-	accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
-	print('accuracy: ', sess.run(accuracy,
-		feed_dict={X: test_x,
-				   Y: test_y}))
+    is_correct = tf.equal(tf.argmax(model, 1), tf.argmax(Y, 1))
+    accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
+    print('accuracy: ', sess.run(accuracy, feed_dict={X: test_x, Y: test_y}))
--- data/prepare.py
+++ data/prepare.py
@@ -4,97 +4,96 @@ import csv
 import numpy as np
 
 
-
 def extract(data):
-	path = "_data/"
-	base = path + data["fileName"].split('_')[1] + "/frames/"
+    path = "_data/"
+    base = path + data["fileName"].split('_')[1] + "/frames/"
 
-	if not(os.path.exists(base)):
-		os.makedirs(base)
+    if not(os.path.exists(base)):
+        os.makedirs(base)
 
-	label = data["fileName"].split("_")[1]
+    label = data["fileName"].split("_")[1]
 
-	video_name = path + data["fileName"].split('_')[1] + "/" + data["fileName"] + "_uncomp.avi"
+    video_name = path + data["fileName"].split('_')[1] + "/" + data["fileName"] + "_uncomp.avi"
 
-	vidcap = cv2.VideoCapture(video_name)
-	success, image = vidcap.read()
+    vidcap = cv2.VideoCapture(video_name)
+    success, image = vidcap.read()
 
-	count = 0
-	success = True
-	return_data = []
+    count = 0
+    success = True
+    return_data = []
 
-	while success:
-		success, image = vidcap.read()
-		if(count > int(data["start"]) and count < int(data["end"])-10):
-			
-			#cv2.imwrite(base + "/" + data["fileName"] + "_%d.jpg" %count, image)
-			img = np.array(image)
-			
-			return_data.append({"image":img, "label":label})
-		elif(count > int(data["end"])-10):
-			break
-		count = count + 1
+    while success:
+        success, image = vidcap.read()
+        if(count > int(data["start"]) and count < int(data["end"])-10 and count%10==0):
 
-	return return_data
-	#extract farme data["start"] ~ data["end"] in videon_name
-	#form -> frame\tlabel
+#           cv2.imwrite(base + "/" + data["fileName"] + "_%d.jpg" %count, image)
+            img = np.array(image)
+
+            return_data.append({"image":img, "label":label})
+        elif(count > int(data["end"])-10):
+            break
+        count = count + 1
+
+    return return_data
+# extract farme data["start"] ~ data["end"] in videon_name
+# form -> frame\tlabel
 
 
-
-#_data/running/person01_running_d1_uncomp.avi
+# _data/running/person01_running_d1_uncomp.avi
 def prepare():
-	path = "_data/"
-	data_seq = "_data/frame_sequence.txt"
+    path = "_data/"
+    data_seq = "_data/frame_sequence.txt"
 
-	form = {"fileName":None, "start":None, "end":None}
+    form = {"fileName":None, "start":None, "end":None}
 
-	lines = [line.rstrip('\n').rstrip('\r').split("\t") for line in open(data_seq)]
-	label = [l[0].rstrip() for l in lines]
-	_frame = [l[-1].split(", ") for l in lines]
-	result = []
+    lines = [line.rstrip('\n').rstrip('\r').split("\t") for line in open(data_seq)]
+    label = [l[0].rstrip() for l in lines]
+    _frame = [l[-1].split(", ") for l in lines]
+    result = []
 
-	for f, l in zip(_frame, label):
-		for ff in f:
-			start = ff.split("-")[0]
-			end = ff.split("-")[1]
-			form = {"fileName" : l, "start":start, "end":end}
-			result.append(form)
+    for f, l in zip(_frame, label):
+        for ff in f:
+            start = ff.split("-")[0]
+            end = ff.split("-")[1]
+            form = {"fileName" : l, "start":start, "end":end}
+            result.append(form)
 
-	d = []
-	count = 1
-	for data in result:
-		print(data["fileName"])
-		d = d + extract(data)
-		if(count == 30):
-			break
-		count = count + 1
-	
+    d = []
+    count = 1
+    for data in result:
+        print(data["fileName"])
+        if not("box" in data["fileName"]):
+            d = d + extract(data)
+            if(count == 50):
+                break
+            count = count + 1
 
-	#[
-	#{"image" : [[[],[],[]]], "label" : label}
-	#{"image" : [[[],[],[]]], "label" : label}
-	#{"image" : [[[],[],[]]], "label" : label}...
-	#]
-	return d
+
+# [
+# {"image" : [[[],[],[]]], "label" : label}
+# {"image" : [[[],[],[]]], "label" : label}
+# {"image" : [[[],[],[]]], "label" : label}...
+# ]
+    return d
+
 
 def make_train_data(data):
+    length_data = len(data)
 
-	length_data = len(data)
+    x = np.array([d["image"] for d in data])
+    _y = np.array([d["label"] for d in data])
 
-	x = np.array([d["image"] for d in data])
-	_y = np.array([d["label"] for d in data])
-
-	y = np.zeros((len(_y), len(set(_y))))
-	y[np.arange(len(_y)), [list(set(_y)).index(i) for i in _y]] = 1
-
-
-	l = int(length_data/10)
-	
-	train_x = x[:l]
-	train_y = y[:l]
-
-	test_x = x[l:]
-	test_y = y[l:]
+    y = np.zeros((len(_y), len(set(_y))))
+    y[np.arange(len(_y)), [list(set(_y)).index(i) for i in _y]] = 1
 
 
-	return [train_x, train_y, test_x, test_y]
+    l = int(length_data/10)
+
+    train_x = x[l:]
+    train_y = y[l:]
+
+    test_x = x[:l]
+    test_y = y[:l]
+
+
+    return [train_x, train_y, test_x, test_y]
